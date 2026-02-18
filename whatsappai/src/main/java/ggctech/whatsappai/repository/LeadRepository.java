33e577d49ca076f2d1d51d0c03a2fa445371e83b
gabrielgc2003 <gabrielgarciaacouto@gmail.com>
2026-02-18 20:59:43 -0300

    ajustes do state service 14

--- whatsappai/src/main/java/ggctech/whatsappai/repository/LeadRepository.java
+++ whatsappai/src/main/java/ggctech/whatsappai/repository/LeadRepository.java
@@ -5,7 +5,6 @@ import ggctech.whatsappai.domain.lead.Lead;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.ScopedValue;
 import java.util.Optional;
 import java.util.UUID;
 
